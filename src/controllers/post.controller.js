84207784f10264b049264dff8084dc3b3151f9f6
leanhvu21042001@gmail.com <leanhvu1412>
2021-10-05 22:03:57 +0700

    Setup controllers

---
+++ src/controllers/post.controller.js
@@ -0,0 +1,13 @@
+const PostController = {}
+
+PostController.addPost = function () { }
+
+PostController.deletePostById = function () { }
+
+PostController.updatePostById = function () { }
+
+PostController.getOnePostById = function () { }
+
+PostController.getAllPosts = function () { }
+
+module.exports = PostController;
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,13 @@
+const UserController = {}
+
+UserController.addUser = function (res, req, next) { }
+
+UserController.deleteUserById = function (res, req, next) { }
+
+UserController.updateUserById = function (res, req, next) { }
+
+UserController.getOneUserById = function (res, req, next) { }
+
+UserController.getAllUsers = function (res, req, next) { }
+
+module.exports = UserController;
